80e3b9d51dc5bdde18e0d882ca4608e67e75a8fa
euchariaevi <42248263+euchariaevi@users.noreply.github.com>
2018-08-09 19:56:41 +0100

    Add files via upload

---
+++ Eucharia2.py
@@ -0,0 +1,7 @@
+
+
+name=input("please enter your name \n")
+print("welcome",name)
+age=int(input("please enter your age \n"))
+year=str((100-age) + 2018)
+print(name,"you will be 100 years old in",year)
