caeb446b55b6579982e4e1842cfe7c39253ea436
euchariaevi <42248263+euchariaevi@users.noreply.github.com>
2018-08-09 19:53:03 +0100

    Delete Eucharia2.py

--- Eucharia2.py
+++
@@ -1,7 +0,0 @@
-
-
-name=input("please enter your name \n")
-print("welcome",name)
-age=int(input("please enter your age \n"))
-year=str((100-age) + 2018)
-print((name) + "you will be 100 years in" +(year))
